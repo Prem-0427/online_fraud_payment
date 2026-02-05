88a8264d4ab6207ecf3b296cafd2d924c7424282
prem kumar <mpremkumar426@gmail.com>
2026-02-05 22:49:08 +0530

    First commit - Online Fraud Detection System

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:33878d3ad992acb45c478e0ce0986cd7230fe10c8ae68d072c122b6d5a23b19e
+size 1611
---
+++ train_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:879929e96fe42e511dd77928658ae3cf541d19688e12aec7fdd7d33ef86dccf4
+size 1079
