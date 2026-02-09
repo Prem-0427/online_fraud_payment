81b98d0777a200c898e5350babd2c41a1a2e64ed
prem kumar <mpremkumar426@gmail.com>
2026-02-09 14:28:52 +0530

    Add requirements file

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33878d3ad992acb45c478e0ce0986cd7230fe10c8ae68d072c122b6d5a23b19e
-size 1611
+oid sha256:8c039e51bddd452792923da326fe495cfc8e5f43efe72e2380339fc4d29bc5d8
+size 1626
